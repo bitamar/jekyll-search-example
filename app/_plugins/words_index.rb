cdd5272497ece15d844a6b2d88fb9e7533608c06
Itamar Shapira Bar-Lev <bitamar@gmail.com>
2014-07-29 05:47:34 +0300

    Creating words index

--- app/_plugins/words_index.rb
+++ app/_plugins/words_index.rb
@@ -1,12 +1,22 @@
 module Reading
   class Generator < Jekyll::Generator
     def generate(site)
-      puts 'hoyhoyhoyhoyhoyhoyhoyhoyhoyhoyhoyhoyhoyhoyhoyhoyhoyhoy'
+      words_index = Hash.new
+
       site.posts.to_enum.with_index(1).each do |post, i|
-        post.content.split.uniq.each do |word|
-          puts word
+        post.content.downcase.gsub('-', ' ').gsub(/[^a-z\s]/i, '').split.uniq.sort.each do |word|
+          if word.length < 2
+            next
+          end
+
+          if !words_index.has_key?(word)
+            words_index[word] = Array.new
+          end
+          words_index[word].push(i)
         end
       end
+
+      File.write('words.json', words_index.to_json)
     end
   end
 end
